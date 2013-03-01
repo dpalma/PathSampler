903d7fa6331e0518f39099b7db0f7206abef7921
Daniel Palma <daniel_palma@yahoo.com>
2013-02-28 21:59:16 -0500

    Extracted MouseController class from PassabilityController

---
+++ Views/MouseController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using PathFind.ViewModels;
+using PathFind.Models;
+
+namespace PathFind.Views
+{
+   abstract class MouseController
+   {
+      private FrameworkElement m_view;
+      public FrameworkElement View
+      {
+         get { return m_view; }
+      }
+
+      private MapVM m_mapViewModel;
+      public MapVM MapViewModel
+      {
+         get { return m_mapViewModel; }
+      }
+
+      public MouseController(FrameworkElement view, MapVM mapViewModel)
+      {
+         m_view = view;
+         m_mapViewModel = mapViewModel;
+
+         View.MouseLeftButtonDown += new System.Windows.Input.MouseButtonEventHandler(MapView_MouseLeftButtonDown);
+         View.MouseLeftButtonUp += new System.Windows.Input.MouseButtonEventHandler(MapView_MouseLeftButtonUp);
+         View.MouseMove += new System.Windows.Input.MouseEventHandler(MapView_MouseMove);
+         View.LostMouseCapture += new System.Windows.Input.MouseEventHandler(MapView_LostMouseCapture);
+      }
+
+      private bool m_mouseDragging = false;
+      public bool MouseDragging
+      {
+         get { return m_mouseDragging; }
+         private set
+         {
+            m_mouseDragging = value;
+            if (!m_mouseDragging)
+            {
+               MapViewModel.SelectedCells.Clear();
+               View.InvalidateVisual();
+            }
+         }
+      }
+
+      void MapView_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+      {
+         if (View.CaptureMouse())
+         {
+            HitTestAndSelect(e);
+            MouseDragging = true;
+         }
+      }
+
+      void MapView_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+      {
+         if (MouseDragging)
+         {
+            Finish();
+
+            View.ReleaseMouseCapture();
+            MouseDragging = false;
+         }
+      }
+
+      private void HitTestAndSelect(System.Windows.Input.MouseEventArgs mouseEventArgs)
+      {
+         GridCoordinate hitCell = MapViewModel.GetHitCell(mouseEventArgs);
+
+         if (hitCell != null)
+         {
+            SelectCell(hitCell);
+         }
+      }
+
+      protected abstract void Finish();
+
+      protected abstract void SelectCell(GridCoordinate cell);
+
+      void MapView_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+      {
+         if (MouseDragging)
+         {
+            HitTestAndSelect(e);
+         }
+      }
+
+      void MapView_LostMouseCapture(object sender, System.Windows.Input.MouseEventArgs e)
+      {
+         MouseDragging = false;
+      }
+   }
+}
--- Views/PassabilityController.cs
+++ Views/PassabilityController.cs
@@ -4,90 +4,24 @@ using System.Windows;
 
 namespace PathFind.Views
 {
-   class PassabilityController
+   class PassabilityController : MouseController
    {
-      private FrameworkElement m_view;
-      public FrameworkElement View
-      {
-         get { return m_view; }
-      }
-
-      private MapVM m_mapViewModel;
-      public MapVM MapViewModel
-      {
-         get { return m_mapViewModel; }
-      }
-
       public PassabilityController(FrameworkElement view, MapVM mapViewModel)
+         : base(view, mapViewModel)
       {
-         m_view = view;
-         m_mapViewModel = mapViewModel;
-
-         View.MouseLeftButtonDown += new System.Windows.Input.MouseButtonEventHandler(MapView_MouseLeftButtonDown);
-         View.MouseLeftButtonUp += new System.Windows.Input.MouseButtonEventHandler(MapView_MouseLeftButtonUp);
-         View.MouseMove += new System.Windows.Input.MouseEventHandler(MapView_MouseMove);
-         View.LostMouseCapture += new System.Windows.Input.MouseEventHandler(MapView_LostMouseCapture);
       }
 
-      private bool m_mouseDragging = false;
-      public bool MouseDragging
+      override protected void Finish()
       {
-         get { return m_mouseDragging; }
-         private set
+         MapViewModel.SetPassability(MapViewModel.SelectedCells, 1);
+      }
+
+      override protected void SelectCell(GridCoordinate cell)
+      {
+         if (MapViewModel.SelectedCells.Add(cell))
          {
-            m_mouseDragging = value;
-            if (!m_mouseDragging)
-            {
-               MapViewModel.SelectedCells.Clear();
-               View.InvalidateVisual();
-            }
+            View.InvalidateVisual();
          }
-      }
-
-      void MapView_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-      {
-         if (View.CaptureMouse())
-         {
-            HitTestAndAddSelected(e);
-            MouseDragging = true;
-         }
-      }
-
-      void MapView_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-      {
-         if (MouseDragging)
-         {
-            MapViewModel.SetPassability(MapViewModel.SelectedCells, 1);
-
-            View.ReleaseMouseCapture();
-            MouseDragging = false;
-         }
-      }
-
-      private void HitTestAndAddSelected(System.Windows.Input.MouseEventArgs mouseEventArgs)
-      {
-         GridCoordinate hitCell = MapViewModel.GetHitCell(mouseEventArgs);
-
-         if (hitCell != null)
-         {
-            if (MapViewModel.SelectedCells.Add(hitCell))
-            {
-               View.InvalidateVisual();
-            }
-         }
-      }
-
-      void MapView_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-      {
-         if (MouseDragging)
-         {
-            HitTestAndAddSelected(e);
-         }
-      }
-
-      void MapView_LostMouseCapture(object sender, System.Windows.Input.MouseEventArgs e)
-      {
-         MouseDragging = false;
       }
    }
 }
