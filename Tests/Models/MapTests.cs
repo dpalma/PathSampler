c303474e052c07fb63282efffd560afad25774ec
Daniel Palma <daniel_palma@yahoo.com>
2013-03-03 12:03:21 -0500

    Added a unit test project

---
+++ Tests/Models/MapTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PathFind.Models;
+
+namespace PathFindTests.Models
+{
+   class MapTests
+   {
+      [Test]
+      public void TestTopLeftCellHasOnlyThreeNeighbors()
+      {
+         Map map = new Map();
+         GridCoordinate[] neighbors = map.GetNeighbors(new GridCoordinate());
+         Assert.AreEqual(3, neighbors.Length);
+      }
+   }
+}
