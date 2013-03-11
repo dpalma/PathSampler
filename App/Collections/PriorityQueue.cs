d2632aa765d053e2f51db511ca9c13b05b32dd40
Daniel Palma <daniel_palma@yahoo.com>
2013-03-11 10:24:07 -0400

    Added a PriorityQueue class

---
+++ App/Collections/PriorityQueue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PathFind.Collections
+{
+   public class PriorityQueue<T> : ICollection where T : IComparable<T>
+   {
+      private List<T> m_list = new List<T>();
+
+      public void Enqueue(T value)
+      {
+         m_list.HeapAdd(value);
+      }
+
+      public T Dequeue()
+      {
+         if (m_list.Count == 0)
+         {
+            throw new InvalidOperationException("Queue is empty");
+         }
+         return m_list.HeapRemove();
+      }
+
+      public T Peek()
+      {
+         if (m_list.Count == 0)
+         {
+            throw new InvalidOperationException("Queue is empty");
+         }
+         return m_list[0];
+      }
+
+      #region ICollection Members
+
+      public void CopyTo(Array array, int index)
+      {
+         throw new NotImplementedException();
+      }
+
+      public int Count
+      {
+         get { return m_list.Count; }
+      }
+
+      public bool IsSynchronized
+      {
+         get { return false; }
+      }
+
+      public object SyncRoot
+      {
+         get { throw new NotImplementedException(); }
+      }
+
+      #endregion
+
+      #region IEnumerable Members
+
+      public IEnumerator GetEnumerator()
+      {
+         return m_list.GetEnumerator();
+      }
+
+      #endregion
+   }
+}
---
+++ Tests/Collections/PriorityQueueTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PathFind.Collections;
+
+namespace PathFindTests.Collections
+{
+   class PriorityQueueTests
+   {
+      [Test]
+      public void TestEnqueueUpdatesCount()
+      {
+         PriorityQueue<int> queue = new PriorityQueue<int>();
+         queue.Enqueue(99);
+         Assert.AreEqual(1, queue.Count);
+      }
+
+      [Test]
+      public void TestDequeueUpdatesCount()
+      {
+         PriorityQueue<int> queue = new PriorityQueue<int>();
+         queue.Enqueue(99);
+         queue.Enqueue(88);
+         Assert.AreEqual(2, queue.Count);
+         queue.Dequeue();
+         Assert.AreEqual(1, queue.Count);
+         queue.Dequeue();
+         Assert.AreEqual(0, queue.Count);
+      }
+
+      [Test]
+      [ExpectedException(typeof(InvalidOperationException))]
+      public void TestDequeueThrowsExceptionForEmptyQueue()
+      {
+         PriorityQueue<int> queue = new PriorityQueue<int>();
+         queue.Dequeue();
+      }
+
+      [Test]
+      public void TestPeekDoesNotChangeCount()
+      {
+         PriorityQueue<int> queue = new PriorityQueue<int>();
+         queue.Enqueue(1);
+         queue.Enqueue(2);
+         Assert.AreEqual(2, queue.Peek());
+      }
+
+      [Test]
+      [ExpectedException(typeof(InvalidOperationException))]
+      public void TestPeekThrowsExceptionForEmptyQueue()
+      {
+         PriorityQueue<int> queue = new PriorityQueue<int>();
+         queue.Peek();
+      }
+
+      [Test]
+      public void TestGetEnumeratorReturnsAllItems()
+      {
+         PriorityQueue<int> queue = new PriorityQueue<int>();
+         for (int i = 10; i <= 100; i += 10)
+         {
+            queue.Enqueue(i);
+         }
+         int itemsEnumerated = 0;
+         IEnumerator enumerator = queue.GetEnumerator();
+         while (enumerator.MoveNext())
+         {
+            Assert.IsTrue(((int)enumerator.Current % 10) == 0);
+            itemsEnumerated++;
+         }
+         Assert.AreEqual(queue.Count, itemsEnumerated);
+      }
+   }
+}
