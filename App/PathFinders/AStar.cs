fc979ec2cf3325e13dcf518ddbd743213c4d3618
Daniel Palma <daniel_palma@yahoo.com>
2013-03-11 16:22:40 -0400

    Added A* implementation stub (no heuristic in use yet)

---
+++ App/PathFinders/AStar.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using PathFind.Collections;
+using PathFind.Core;
+using PathFind.Models;
+
+namespace PathFind.PathFinders
+{
+   internal sealed class AStarNode : IComparable<AStarNode>
+   {
+      public AStarNode(GridCoordinate cell)
+      {
+         m_cell = cell;
+      }
+
+      public GridCoordinate Cell { get { return m_cell; } }
+      private GridCoordinate m_cell;
+
+      public double CostFromStart
+      {
+         get { return m_costFromStart; }
+         internal set { m_costFromStart = value; }
+      }
+      private double m_costFromStart = 0;
+
+      public double CostToGoal
+      {
+         get { return m_costToGoal; }
+         internal set { m_costToGoal = value; }
+      }
+      private double m_costToGoal = Double.PositiveInfinity;
+
+      #region IComparable<AStarNode> Members
+
+      public int CompareTo(AStarNode other)
+      {
+         // TODO: compare by priority
+         return Cell.Equals(other) ? 0 : -1;
+      }
+
+      #endregion
+   }
+
+   public class AStar : PathFinder
+   {
+      public AStar(Map map, ICellColoring cellColoring)
+         : base(map, cellColoring)
+      {
+         OpenList.Enqueue(new AStarNode(Map.Start) { CostToGoal = Map.Start.ManhattanDistance(Map.Goal) });
+      }
+
+      private PriorityQueue<AStarNode> OpenList { get { return m_openList; } }
+      private PriorityQueue<AStarNode> m_openList = new PriorityQueue<AStarNode>();
+
+      private void AddToOpenList(GridCoordinate cell)
+      {
+         OpenList.Enqueue(new AStarNode(cell));
+         CellColoring.SetCellColor(cell, Brushes.Orange);
+      }
+
+      private void AddToClosedList(GridCoordinate cell)
+      {
+         ClosedList.Add(cell);
+         CellColoring.SetCellColor(cell, Brushes.Gray);
+      }
+
+      private bool IsOnOpenList(GridCoordinate cell)
+      {
+         return OpenList.Where(n => n.Cell.Equals(cell)).Count() > 0;
+      }
+
+      protected bool IsUnseen(GridCoordinate cell)
+      {
+         return !IsOnOpenList(cell) && !ClosedList.Contains(cell);
+      }
+
+      public override void Step()
+      {
+         if (OpenList.Count == 0)
+         {
+            // done, no path found
+            Result = PathFindResult.PathNotFound;
+         }
+         else
+         {
+            AStarNode node = OpenList.Dequeue();
+            AddToClosedList(node.Cell);
+
+            if (node.Cell.Equals(Map.Goal))
+            {
+               // done, path found
+               if (BuildPath())
+               {
+                  Result = PathFindResult.PathFound;
+               }
+            }
+            else
+            {
+               GridCoordinate[] neighbors = Map.GetNeighbors(node.Cell);
+
+               foreach (var neighbor in neighbors)
+               {
+                  //double newCost = node.CostFromStart + node.Cell.ManhattanDistance(neighbor);
+
+                  if (IsUnseen(neighbor))
+                  {
+                     AddToOpenList(neighbor);
+                     Predecessors[neighbor] = node.Cell;
+                  }
+               }
+            }
+         }
+      }
+   }
+}
---
+++ Tests/PathFinders/AStarTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PathFind.Core;
+using PathFind.Models;
+using PathFind.PathFinders;
+
+namespace PathFindTests.PathFinders
+{
+   internal class NullCellColoring : ICellColoring
+   {
+      public void SetCellColor(GridCoordinate cell, System.Windows.Media.Brush brush)
+      {
+      }
+   }
+
+   class AStarTests
+   {
+      private static Map BuildMap(int size)
+      {
+         Map map = new Map()
+         {
+            RowCount = size,
+            ColumnCount = size,
+            Goal = new GridCoordinate() { Row = size - 1, Column = size - 1 },
+         };
+         return map;
+      }
+
+      private static void BlockRow(Map map, int row)
+      {
+         for (int i = 0; i < map.ColumnCount; ++i)
+         {
+            map.BlockedCells[new GridCoordinate() { Row = row, Column = i }] = 1;
+         }
+      }
+
+      private static PathFindResult FindPath(PathFinder pathFinder)
+      {
+         while (pathFinder.Result == null)
+         {
+            pathFinder.Step();
+         }
+         return pathFinder.Result.Value;
+      }
+
+      [Test]
+      public void TestPathFoundOnTrivialMap()
+      {
+         Map map = BuildMap(3);
+         AStar astar = new AStar(map, new NullCellColoring());
+         Assert.AreEqual(PathFindResult.PathFound, FindPath(astar));
+      }
+
+      [Test]
+      public void TestNoPathFoundWhenBlocked()
+      {
+         Map map = BuildMap(3);
+         BlockRow(map, 1);
+         AStar astar = new AStar(map, new NullCellColoring());
+         Assert.AreEqual(PathFindResult.PathNotFound, FindPath(astar));
+      }
+   }
+}
