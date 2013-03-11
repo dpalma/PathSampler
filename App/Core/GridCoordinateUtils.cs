87336ab4bfe929093c90385b07d8b12a72cb085b
Daniel Palma <daniel_palma@yahoo.com>
2013-03-11 13:29:47 -0400

    Added extension methods for GridCoordinate in GridCoordinateUtils

---
+++ App/Core/GridCoordinateUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PathFind.Core
+{
+   public static class GridCoordinateUtils
+   {
+      public static double EuclideanDistance(this GridCoordinate from, GridCoordinate to)
+      {
+         return Math.Sqrt(
+            ((to.Column - from.Column) * (to.Column - from.Column)) +
+            ((to.Row - from.Row) * (to.Row - from.Row))
+            );
+      }
+
+      public static double ManhattanDistance(this GridCoordinate from, GridCoordinate to)
+      {
+         return Math.Abs(to.Column - from.Column) + Math.Abs(to.Row - from.Row);
+      }
+   }
+}
