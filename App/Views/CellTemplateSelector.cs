2541b1dd59dc8587be7d702e05434f5513a8bafe
Daniel Palma <daniel_palma@yahoo.com>
2013-03-27 14:25:59 -0400

    Added a data template selector for MapView, CellTemplateSelector

---
+++ App/Views/CellTemplateSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using PathFind.ViewModels;
+
+namespace PathFind.Views
+{
+   public class CellTemplateSelector : DataTemplateSelector
+   {
+      public override System.Windows.DataTemplate SelectTemplate(object item, System.Windows.DependencyObject container)
+      {
+         CellVM cellVM = item as CellVM;
+         if (cellVM != null)
+         {
+            if (cellVM.IsBlocked)
+            {
+               return BlockedCellTemplate;
+            }
+
+            return BaseCellTemplate;
+         }
+
+         return base.SelectTemplate(item, container);
+      }
+
+      public DataTemplate BaseCellTemplate { get; set; }
+
+      public DataTemplate BlockedCellTemplate { get; set; }
+   }
+}
